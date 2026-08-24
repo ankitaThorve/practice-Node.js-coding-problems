a45989b5205bb443ef607f38e4c354f94036df4b
Ankita <ankietathorve@gmail.com>
2026-08-24 23:19:01 +0530

    Convert a temperature from Celsius to Fahrenheit.

---
+++ w3s-node.js/Celsius-to-Fahrenheit.js
@@ -0,0 +1,6 @@
+const celsius = 25;
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 9) / 5 + 32;
+// print the result
+console.log(celsius + " Celsius = " + fahrenheit.toFixed(1) + " Fahrenheit");
