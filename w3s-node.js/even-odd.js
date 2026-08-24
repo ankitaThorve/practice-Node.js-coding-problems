a6babcd365137e76cf0c0eb0ceccd0c9df83779d
Ankita <ankietathorve@gmail.com>
2026-08-24 23:34:12 +0530

    Check if a number is even or odd.

---
+++ w3s-node.js/even-odd.js
@@ -0,0 +1,8 @@
+// const n = Math.floor(Math.random() * (max - min + 1)) + min;
+// const n = Math.random() * (max - min) + min;
+// const n = Math.floor(Math.random())
+const n = 78;
+
+// check if even or odd and print
+console.log(n % 2 === 0 ? "even" : "odd");
+console.log(n);
