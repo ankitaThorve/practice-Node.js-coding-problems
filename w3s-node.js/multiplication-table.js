605075fd260bb227c2c3ef4888d5ccd2dae61633
Ankita <ankietathorve@gmail.com>
2026-08-25 23:32:59 +0530

    Print the multiplication table for a given number.

--- w3s-node.js/multiplication-table.js
+++ w3s-node.js/multiplication-table.js
@@ -1,6 +1,6 @@
 const n = 9;
 
 // print multipliaction table
-for (let index = 1; index <= 10; index++) {
+for (let index = 1; index <= 10; index++) { // loop runs from 1 to 10
   console.log(`${n} x ${index} = ${n * index}`);
 }
