c833a8e5e50290ef736bac2305a7f913aaecef76
Ankita <ankietathorve@gmail.com>
2026-08-25 23:04:11 +0530

    Convert a score into a letter grade.

---
+++ grade-calculator.js
@@ -0,0 +1,14 @@
+const score = 67;
+
+// print the grade
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else if (score >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
