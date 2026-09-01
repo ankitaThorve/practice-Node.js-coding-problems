1dbbb09931fdea63fb38c61a988272bd988cf5c9
Ankita <ankietathorve@gmail.com>
2026-09-01 23:41:47 +0530

    Read a list of numbers and calculate their sum, using for...of loop

--- w3s-node.js/sum-of-numbers.js
+++ w3s-node.js/sum-of-numbers.js
@@ -1,12 +1,21 @@
-const lines = require("fs")
-  .readFileSync("/dev/stdin", "utf8")
-  .trim()
-  .split("\n");
-const n = parseInt(lines[0].trim());
-let sum = 0;
-// Read numbers and calculate sum
-for (let i = 0; i < n; i++) {
-  sum += parseInt(lines[i + 1].trim());
+// const lines = require("fs")
+//   .readFileSync("/dev/stdin", "utf8")
+//   .trim()
+//   .split("\n");
+// const n = parseInt(lines[0].trim());
+// let sum = 0;
+// // Read numbers and calculate sum
+// for (let i = 0; i < n; i++) {
+//   sum += parseInt(lines[i + 1].trim());
+// }
+// // Print the sum
+// console.log("Sum: " + sum);
+
+// using for...of loop
+let numbers = [10, 20, 30, 40];
+let sum1 = 0;
+for (const num of numbers) {
+  sum1 += num;
 }
-// Print the sum
-console.log("Sum: " + sum);
+
+console.log(sum1);
